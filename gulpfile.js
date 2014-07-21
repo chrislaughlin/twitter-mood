caddcb68c39ec5c724fd470bf19781fd894beb8b
Chris Laughlin <bluntm@gmail.com>
2014-07-21 21:02:18 +0100

    adding more tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var jasmine = require('gulp-jasmine');
 
 gulp.task('default', function() {
 
+});
+
+gulp.task('test:unit', function () {
+    gulp.src('server/test/unit/*-spec.js')
+        .pipe(jasmine());
 });
--- server/test/unit/twitter-spec.js
+++ server/test/unit/twitter-spec.js
@@ -8,7 +8,16 @@ describe("Twitter Status Tests", function() {
 
     it('should return results for \'Belfast\' search term', function(done) {
         twitter.search('Belfast', function(results) {
-            expect(results).toBeDefined();
+            var tweets = results;
+            expect(tweets).toBeDefined();
+            expect(tweets.statuses.length).toBeGreaterThan(0); //All based on twitter :)
+            done();
+        });
+    });
+
+    it('should return undefined if the search term is not passed in', function(done) {
+        twitter.search(undefined, function(results) {
+            expect(results).toBeUndefined();
             done();
         });
     })

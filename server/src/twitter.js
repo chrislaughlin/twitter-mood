f36eca5d50785fb924a58a4b9c0c37e31eea0d18
Chris Laughlin <bluntm@gmail.com>
2014-07-21 20:54:58 +0100

    adding node jasmine and some basic tests

---
+++ server/src/twitter.js
@@ -0,0 +1,34 @@
+var Twit = require('twit');
+var config = require('./config/config');
+var T = new Twit({
+    consumer_key:         'j7e73ODaTmBLCjTebVuLyQ'
+    , consumer_secret:      'kUHs6yPcG2snHAx70tcJrzqgkU3jDwVzrmQ8AsLfvI'
+    , access_token:         '1952367470-wId4DkQc10jexSuUgg73Xo29Fjo53yCtc3Mzzkf'
+    , access_token_secret:  'vewT5P13RbW3JnLvLdnsgKYKEdWFNpW9xz9Iv85alJcvu'
+});
+
+/*
+ Returns the simple string 'Hello'
+ */
+exports.hello = function() {
+    return 'Hello World';
+};
+
+/*
+ Searches for the last ten tweets containing the passed in query param
+ */
+exports.search = function(searchTerm, callBack) {
+    //Check that a search has been sent in request query
+    if (searchTerm != undefined) {
+        T.get('search/tweets', { q: searchTerm, count: 10 }, function(err, reply) {
+            if (err) {
+                console.dir(err);
+                callBack(undefined);
+            } else {
+                callBack(reply);
+            }
+        });
+    } else {
+        callBack(undefined);
+    }
+}
--- server/test/unit/twitter-spec.js
+++ server/test/unit/twitter-spec.js
@@ -1,1 +1,15 @@
+describe("Twitter Status Tests", function() {
 
+    var twitter = require('../../src/twitter');
+
+    it("should return hello world", function() {
+        expect(twitter.hello()).toBe('Hello World');
+    });
+
+    it('should return results for \'Belfast\' search term', function(done) {
+        twitter.search('Belfast', function(results) {
+            expect(results).toBeDefined();
+            done();
+        });
+    })
+});

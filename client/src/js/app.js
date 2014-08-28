02b3a411b60290122f69d2b9c64758d1d9545ac1
Chris Laughlin <bluntm@gmail.com>
2014-08-28 22:26:32 +0100

    adding spinner

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -3,14 +3,14 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('twitterMood', [
-  'ngRoute',
-  'twitterMood.filters',
-  'twitterMood.services',
-  'twitterMood.directives',
-  'twitterMood.controllers'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: 'MoodCtrl'});
-  $routeProvider.when('/results/:username', {templateUrl: 'partials/results.html', controller: 'ResultsCtrl'});
-  $routeProvider.otherwise({redirectTo: '/main'});
+    'ngRoute',
+    'twitterMood.filters',
+    'twitterMood.services',
+    'twitterMood.directives',
+    'twitterMood.controllers'
+]).config(['$routeProvider', function ($routeProvider) {
+    // Routes
+    $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: 'MoodCtrl'});
+    $routeProvider.when('/results/:username', {templateUrl: 'partials/results.html', controller: 'ResultsCtrl'});
+    $routeProvider.otherwise({redirectTo: '/main'});
 }]);
--- client/src/js/controllers.js
+++ client/src/js/controllers.js
@@ -3,31 +3,32 @@
 /* Controllers */
 
 angular.module('twitterMood.controllers', [])
-  .controller('MoodCtrl', ['$scope', '$location', function($scope, $location) {
+    .controller('MoodCtrl', ['$scope', '$location', function ($scope, $location) {
 
         $scope.name = '';
 
-        $scope.submit = function(name) {
+        $scope.submit = function (name) {
             if ($scope.name == '') {
                 alert('You must provide a username.');
                 return;
             }
-            $location.path('results/'+$scope.name);
+            $location.path('results/' + $scope.name);
         }
 
-  }])
-  .controller('ResultsCtrl', ['$scope', '$routeParams', '$http', function($scope, $routeParams, $http) {
+    }])
+    .controller('ResultsCtrl', ['$scope', '$routeParams', '$http',
+        function ($scope, $routeParams, $http) {
 
-        $scope.results = {};
-        $scope.loading = true;
-        $http.get('/score/'+$routeParams.username).success(function(data) {
-            $scope.results = {
-                username: $routeParams.username,
-                mood: data.totalPositive > data.totalNegative ? 'happy' : 'sad',
-                tweetCount: data.totalTweetCount,
-                happyCount: data.totalPositive,
-                sadCount: data.totalNegative
-            };
-            $scope.loading = false;
-        });
-  }]);
+            $scope.results = {};
+            $scope.loading = true;
+            $http.get('/score/' + $routeParams.username).success(function (data) {
+                $scope.results = {
+                    username: $routeParams.username,
+                    mood: data.totalPositive > data.totalNegative ? 'happy' : 'sad',
+                    tweetCount: data.totalTweetCount,
+                    happyCount: data.totalPositive,
+                    sadCount: data.totalNegative
+                };
+                $scope.loading = false;
+            });
+        }]);

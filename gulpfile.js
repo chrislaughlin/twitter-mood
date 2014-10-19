7b26cf65d71ffb82cbbadeeba24c12c274388059
Chris Laughlin <bluntm@gmail.com>
2014-10-19 20:39:34 +0100

    adding develop task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-var gulp    = require('gulp');
-var jasmine = require('gulp-jasmine');
+var gulp        = require('gulp');
+var jasmine     = require('gulp-jasmine');
+var nodemon     = require('gulp-nodemon');
+var jshint      = require('gulp-jshint');
 
 gulp.task('default', function() {
 
@@ -9,3 +11,16 @@ gulp.task('test:unit', function () {
     gulp.src('client/test/unit/*Spec.js')
         .pipe(jasmine());
 });
+
+gulp.task('lint', function () {
+    gulp.src('./**/*.js')
+        .pipe(jshint())
+});
+
+gulp.task('develop', function () {
+    nodemon({ script: 'server/src/server.js', ext: 'html js css', ignore: ['server/test/**/*.js', 'client/test/**/*.js'] })
+        .on('change', ['lint'])
+        .on('restart', function () {
+            console.log('restarted!')
+        })
+})
